f21361dde77d2a2006e7687bb911938a503c979d
ta-ke-sh1 <trunght.yrc@gmail.com>
2026-06-01 14:35:40 +0700

    Added navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,11 @@ import "@mantine/notifications/styles.css";
 import "@mantine/dates/styles.css";
 import "./styles/app.scss";
 import "./styles/headings.scss"
+import "./styles/menu.css"
 
 import "mantine-datatable/styles.layer.css";
 
-import "@fontsource/space-mono/400.css";
+import "@fontsource/google-sans-flex/400.css";
 import "@fontsource/bricolage-grotesque/200.css";
 
 import {Notifications} from "@mantine/notifications";
@@ -20,7 +21,17 @@ import NotificationWrapper from "./components/wrapper/notification.wrapper.tsx";
 import ProtectedWrapper from "./components/wrapper/protected.wrapper.tsx";
 import {ROLES} from "./enums/auth.ts";
 import DetailsLayout from "./layouts/authenticated/details/details.layout.tsx";
-import NavigationBar from "./components/navigator/navigator.tsx";
+import StaggeredMenu from "./components/menu/side/side.tsx";
+
+const menuItems = [
+    { label: 'TRANG CHỦ', ariaLabel: 'Go to home page', link: '/' },
+    { label: 'GIỚI THIỆU', ariaLabel: 'Learn about us', link: '/about' },
+    { label: 'DỊCH VỤ', ariaLabel: 'View our services', link: '/services' },
+    { label: 'LIÊN HỆ', ariaLabel: 'Get in touch', link: '/contact' }
+];
+
+const socialItems = [
+];
 
 export default function App() {
     const router = createBrowserRouter([
@@ -43,15 +54,30 @@ export default function App() {
     ]);
 
     const theme = createTheme({
-        fontFamily: "Space Mono",
+        fontFamily: "Google Sans Flex",
     });
 
     return (
         <MantineProvider theme={theme} defaultColorScheme={"dark"}>
+            <StaggeredMenu
+                position="right"
+                items={menuItems}
+                socialItems={socialItems}
+                displaySocials
+                displayItemNumbering={true}
+                menuButtonColor="#ffffff"
+                openMenuButtonColor="#fff"
+                changeMenuColorOnOpen={true}
+                colors={['#B497CF', '#5227FF']}
+                logoUrl="/path-to-your-logo.svg"
+                accentColor="#5227FF"
+                onMenuOpen={() => console.log('Menu opened')}
+                onMenuClose={() => console.log('Menu closed')}
+            />
             <Notifications/>
             <ModalsProvider>
                 <NotificationWrapper>
-                    <NavigationBar />
+
                     <RouterProvider router={router}/>
                 </NotificationWrapper>
             </ModalsProvider>
